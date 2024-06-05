fb52091e624febf3c9b5d3b819fd3cd3acb96a47
HeegonChae <heagon72@gmail.com>
2024-06-05 16:49:21 +0900

    create(change_label_number) : Make an.py file

---
+++ yolo/change_label_number.py
@@ -0,0 +1,28 @@
+import os
+
+# 경로
+label_dir = 'diretory'
+
+# 모든 txt 파일에 대해 작업 수행
+for filename in os.listdir(label_dir):
+    if filename.endswith('.txt'):
+        file_path = os.path.join(label_dir, filename)
+        
+        # 파일 읽기
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+        
+        # 첫 번째 숫자를 3에서 0으로 변경
+        new_lines = []
+        for line in lines:
+            parts = line.split()
+            if parts[0] == '0':
+                parts[0] = '1'
+            new_lines.append(' '.join(parts))
+            
+        # 파일 쓰기
+        with open(file_path, 'w') as file:
+            file.write('\n'.join(new_lines))
+
+print("Files have been updated.")
+
