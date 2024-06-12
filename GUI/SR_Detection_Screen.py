4061198dc33b7bc7df7db27109c9a6fcfc185ffb
HeegonChae <heagon72@gmail.com>
2024-06-12 19:45:05 +0900

    feat(SR_Detection_Screen.py) : Make an pair of camera_id and data_directory

--- GUI/SR_Detection_Screen.py
+++ GUI/SR_Detection_Screen.py
@@ -6,19 +6,34 @@ from PyQt5.QtGui import *
 from PyQt5 import uic
 from PyQt5.QtCore import pyqtSignal
 
+# 영상 경로 관련 파라미터
+camera_dirs = {
+        "A" : ['./data/output/detection_A'],
+        "B" : ['./data/output/detection_B', './data/output/lstm_B'],
+        "C" : ['./data/output/detection_C',  './data/output/lstm_C']
+    }
+
 class Detection_Screen(QDialog):   
     closed = pyqtSignal()  # Signal emitted when the window is closed
     
-    def __init__(self, parent=None):                   
+    def __init__(self, camera_id, parent=None):  # 부모 window 설정        
         super(Detection_Screen, self).__init__(parent)       
-        detection_screen_ui = './SR_detection_screen.ui'               #'./src/SR_detection_screen.ui'     
+        detection_screen_ui = './src/SR_detection_screen.ui'            # ./src/gui/SR_detection_screen.ui  
         uic.loadUi(detection_screen_ui, self)
-
-        self.label_detected = self.findChild(QLabel, 'labelDetected')  # QLabel 객체를 가져옵니다. 'imageLabel'은 UI 파일에 정의된 QLabel의 이름입니다.
+        
+        self.label_detected = self.findChild(QLabel, 'labelDetected')  # QLabel 객체를 가져옴
+        self.detection_img_dir = None
+        if camera_id == 'A':
+            self.detection_img_dir = camera_dirs['A'][0]
+        elif camera_id == 'B':
+            self.detection_img_dir = camera_dirs['B'][0]
+        elif camera_id == 'C':
+            self.detection_img_dir = camera_dirs['C'][0]          
         self.Loadlastimage()
 
     def Loadlastimage(self):
-        detection_img_dir = './data/output/detection'
+        detection_img_dir = self.detection_img_dir
+           
         # detection_img_dir 디렉토리에서 모든 파일을 가져옵니다.
         image_files = [f for f in os.listdir(detection_img_dir) if os.path.isfile(os.path.join(detection_img_dir, f))]
         
@@ -43,6 +58,7 @@ if __name__ == '__main__':
     # print(os.getcwd())
 
     app = QApplication(sys.argv)
-    detection_screen = Detection_Screen()
+    detection_screen = Detection_Screen('C')
     detection_screen.show()
     sys.exit(app.exec_())
+
